64738e06ba3fc79912ff158fce6c9e4755153955
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-06-30 10:43:57 +0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Oleksandr Lysenko
         </a>
       </header>
     </div>
